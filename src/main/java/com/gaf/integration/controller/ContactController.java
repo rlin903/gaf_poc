2ba15cdb8207d019829d8c81aedfb80c0ff47083
Richard Lin <richard.lin@gaf.com>
2019-10-25 09:36:18 -0400

    add logger, and change log framework to log4j2

--- src/main/java/com/gaf/integration/controller/ContactController.java
+++ src/main/java/com/gaf/integration/controller/ContactController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 @RestController
@@ -16,16 +19,20 @@ import java.util.List;
         description="Operations pertaining to contact in ERP System")
 public class ContactController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ContactController.class);
+
     @Autowired
     CRUDService dataService;
 
     @GetMapping
     public List<Contact> findAll(){
+        logger.debug("let's find all contacts");
         return dataService.findAllContact();
     }
 
     @GetMapping(path = {"/{id}"})
     public ResponseEntity<Contact> findById(@PathVariable long id) throws ResourceNotFoundException {
+        logger.debug("let's find contact, id={}", id);
         return dataService.findContactById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElseThrow(() -> new ResourceNotFoundException("Contact not found for this id :: " + id));
@@ -33,12 +40,14 @@ public class ContactController {
 
     @PostMapping
     public Contact create(@RequestBody Contact contact){
+        logger.debug("let's create a contact, name={}", contact.getName());
         return dataService.createContact(contact);
     }
 
     @PutMapping(value="/{id}")
     public ResponseEntity<Contact> update(@PathVariable("id") long id,
                                           @RequestBody Contact contact)  throws ResourceNotFoundException {
+        logger.debug("let's update contact, id={}", id);
         return dataService.findContactById(id)
                 .map(record -> {
                     record.setName(contact.getName());
